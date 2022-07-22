134137ffa931c5f6c18489ad3250fa26cd60de60
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-07-22 10:23:23 -0600

    Optimización 3.0

--- src/ItemData.kt
+++ src/ItemData.kt
@@ -1,3 +1,4 @@
+// Lab3. Diego Alexander Hernández Silvestre | Carné: 21270
 data class ItemData (
     var originalPos: Int,
     var originalValue: Any,
@@ -5,22 +6,20 @@ data class ItemData (
     var info: String? = null
 )
 fun main() {
-    val result = processList(listOf(20, 25, 2, 7, "hola", "", true, false, null, 2.0))
+    val result = processList(listOf(20, 25, 2, 7,"hola","", true, false, null, 2.0))
     println(result)
 }
 fun processList(inputList: List<Any?>?): List<ItemData>? {
-    inputList?.let { val finalList = ArrayList<ItemData>()
-        val count: Int = inputList.size - 1
-        for (item in 0.. count) {
-            var type: String? = ""
+    inputList?.let { val finalList = ArrayList<ItemData>() //Filtrar valores nulos
+        val count = inputList.size - 1 // Tamaño de la lista
+        for (item in 0.. count) { var type: String? = "" // Recorrer todos los elementos de la lista
             var info: String? = ""
             val element = inputList[item]
-            when (element) { is Int -> { type = "entero"
-                info = if ((element % 10) == 0) { "M10" } else if ((element % 5) == 0) { "M5" } else if ((element % 2) == 0) { "M2" } else { null } }
+            when (element) { is Int -> { type = "entero" //Filtrar cada elemento dependiendo el tipo.
+                info = when {(element % 10) == 0 -> "M10" ; (element % 5) == 0 -> "M5" ; (element % 2) == 0 -> "M2" else -> null }}
                 is String -> { type = "cadena" ; info = "L" + element.length }
-                is Boolean -> { type = "booleano" ; info = if (element == true) "Verdadero" else "Falso" }
-            }
-            element?.let { finalList.add(ItemData(item, element, type, info)) }
-        }
+                is Boolean -> { type = "booleano" ; info = when (element) { true -> "Verdadero" else -> "Falso" } }
+                else -> {type = null ; info = null } }
+            element?.let { finalList.add(ItemData(item, element, type, info)) } } //Añadir todos los valores a la lista final
         return finalList
     } ?: return null }
