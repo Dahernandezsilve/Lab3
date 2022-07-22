34f762b77012a4bfa5a78cb1291562e1c5ae8c71
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-07-22 09:23:58 -0600

    Optimización de código
    Se detectaron palabras en inglés y español, se procedió a traducirlas al inglés.
    También se utilizaron referencias innecesarias a null, por lo que se utilizó let para evitarlas y además, se eliminaron espacios en blanco y en general se buscó reducir el código.

--- src/ItemData.kt
+++ src/ItemData.kt
@@ -1,60 +1,29 @@
 data class ItemData (
-    // No tocar esta clase ---
-
-        var originalPos: Int,
-        var originalValue: Any,
-        var type: String? = null,
-        var info: String? = null
+    var originalPos: Int,
+    var originalValue: Any,
+    var type: String? = null,
+    var info: String? = null
 )
-    fun main() {
-        val result = processList(listOf(20, 25, 2, 7, "hola", "", true, false, null, 2.0))
-        println(result)
-    }
-    fun processList(inputList: List<Any?>?): List<ItemData>? {
-        if (inputList==null){
-            return null
-        }
-        var listaFinal = ArrayList<ItemData>()
-        var contar:Int = inputList!!.size-1
-        for (item in 0..contar) {
-            var originalPos:Int = item
-            var type:String?=""
-            var info:String?=""
+fun main() {
+    val result = processList(listOf(20, 25, 2, 7, "hola", "", true, false, null, 2.0))
+    println(result)
+}
+fun processList(inputList: List<Any?>?): List<ItemData>? {
+    inputList?.let { val finalList = ArrayList<ItemData>()
+        val count: Int = inputList.size - 1
+        for (item in 0.. count) {
+            val originalPos: Int = item
+            var type: String?
+            var info: String?
             val element = inputList[item]
-            when (element) {
-                is Int -> {
-                    var value:Int = element
+            when (element) { is Int -> { val value: Int = element
                     type = "entero"
-                    if ((value % 10) == 0){
-                        info = "M10"
-                    } else if ((value % 5) == 0){
-                        info = "M5"
-                    } else if ((value % 2) == 0){
-                        info = "M2"
-                    } else {
-                        info = null
-                    }
-                }
-                is String -> {
-                    type = "cadena"
-                    info = "L" + element.length
-                }
-                is Boolean -> {
-                    type = "booleano"
-                    if (element == true){
-                        info = "Verdadero"
-                    } else {
-                        info = "Falso"
-                    }
-                }
-                else -> {
-                    type = null
-                    info = null
-                }
+                    info = if ((value % 10) == 0) { "M10" } else if ((value % 5) == 0) { "M5" } else if ((value % 2) == 0) { "M2" } else { null } }
+                is String -> { type = "cadena" ; info = "L" + element.length }
+                is Boolean -> { type = "booleano" ; info = if (element == true) "Verdadero" else "Falso" }
+                else -> { type = null ; info = null }
             }
-            if (element != null) {
-                listaFinal.add(ItemData(originalPos, (element ?: "null"), type, info))
-            }
+            element?.let { finalList.add(ItemData(originalPos, element, type, info)) }
         }
-        return listaFinal
-    }
+        return finalList
+    } ?: return null }
