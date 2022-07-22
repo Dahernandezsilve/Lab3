92872cb5975299f8f18b9f9c69b581718e6f2d36
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-07-22 09:43:37 -0600

    Optimización 2.0
    Se logró reducir nuevamenta la cantidad de líneas de código.

--- src/ItemData.kt
+++ src/ItemData.kt
@@ -12,18 +12,15 @@ fun processList(inputList: List<Any?>?): List<ItemData>? {
     inputList?.let { val finalList = ArrayList<ItemData>()
         val count: Int = inputList.size - 1
         for (item in 0.. count) {
-            val originalPos: Int = item
-            var type: String?
-            var info: String?
+            var type: String? = ""
+            var info: String? = ""
             val element = inputList[item]
-            when (element) { is Int -> { val value: Int = element
-                    type = "entero"
-                    info = if ((value % 10) == 0) { "M10" } else if ((value % 5) == 0) { "M5" } else if ((value % 2) == 0) { "M2" } else { null } }
+            when (element) { is Int -> { type = "entero"
+                info = if ((element % 10) == 0) { "M10" } else if ((element % 5) == 0) { "M5" } else if ((element % 2) == 0) { "M2" } else { null } }
                 is String -> { type = "cadena" ; info = "L" + element.length }
                 is Boolean -> { type = "booleano" ; info = if (element == true) "Verdadero" else "Falso" }
-                else -> { type = null ; info = null }
             }
-            element?.let { finalList.add(ItemData(originalPos, element, type, info)) }
+            element?.let { finalList.add(ItemData(item, element, type, info)) }
         }
         return finalList
     } ?: return null }
